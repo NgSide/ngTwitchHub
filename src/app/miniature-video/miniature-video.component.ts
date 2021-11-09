71f4795c9c164b724f30aebd21c7d06b0bde925b
MaevaBtd <melizenn@gmail.com>
2021-11-09 17:30:54 +0100

    Set model and api-service

---
+++ src/app/miniature-video/miniature-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-miniature-video',
+  templateUrl: './miniature-video.component.html',
+  styleUrls: ['./miniature-video.component.css']
+})
+export class MiniatureVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/twitch-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Live } from './models/live.model';
+import { HttpClient } from '@angular/common/http';
+
+const STREAM_LIST = 'https://api.twitch.tv/helix/streams';
+const BEARER_AUTH = 'Authorization: Bearer nia7lttt8bnbe14u2qgb7jpftmniva';
+const CLIENT_ID = 'Client-Id: ux0wlqbi147fhhv3nkn8ay5cuf2ui3'
+
+@Injectable()
+export class TwitchApiService {
+  currentLivesList$: Observable<Live[]>;
+
+  constructor(private http: HttpClient) {}
+
+  getCurrentLivesList() {
+    return this.http.get<Live[]>(STREAM_LIST);
+  }
+}
