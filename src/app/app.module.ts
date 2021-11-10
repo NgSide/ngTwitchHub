2bb792f8025a8a59051e3215eff824a3640b8c18
Christopher MONTOYA <contact@christopher-montoya.com>
2021-11-10 01:25:12 +0000

    feat(stream-card): generate default component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StreamThumbnailComponent } from './stream-thumbnail/stream-thumbnail.component';
+import { StreamCardComponent } from './stream-card/stream-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StreamThumbnailComponent
+    StreamThumbnailComponent,
+    StreamCardComponent
   ],
   imports: [
     BrowserModule,
