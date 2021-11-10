437a2096f8fad83a3a9d8ad5bf31417454bb330c
KevMrl <93998423+KevMrl@users.noreply.github.com>
2021-11-10 09:16:42 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Kevin - Angular ' + VERSION.major;
+  name = 'Kevin Mrl- Angular ' + VERSION.major;
 }
