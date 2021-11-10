9098a594bb58461bffb113f42d97ba74f18a4e42
Dozhwal <53612822+kickx74@users.noreply.github.com>
2021-11-10 13:19:48 +0000

    rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { PictureComponent } from './picture/picture.component';
+import { CardComponent } from './cards/card.component';
+import { } from ''
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, PictureComponent ],
+  declarations: [ AppComponent, HelloComponent, CardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
