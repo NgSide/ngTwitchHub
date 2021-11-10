5d4d29558c9475cdf50aaf79a6b7b1ad6b7a9f62
MaevaBtd <melizenn@gmail.com>
2021-11-10 14:31:23 +0100

    5 files modified

--- src/app/twitch-api.service.ts
+++ src/app/twitch-api.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Live } from './models/live.model';
 import { HttpClient } from '@angular/common/http';
+import { Headers, RequestOptions } from '@angular/http';
 
 const STREAM_LIST = 'https://api.twitch.tv/helix/streams';
-const BEARER_AUTH = 'Authorization: Bearer nia7lttt8bnbe14u2qgb7jpftmniva';
-const CLIENT_ID = 'Client-Id: ux0wlqbi147fhhv3nkn8ay5cuf2ui3'
+const BEARER_AUTH = 'Bearer nia7lttt8bnbe14u2qgb7jpftmniva';
+const CLIENT_ID = 'ux0wlqbi147fhhv3nkn8ay5cuf2ui3';
 
 @Injectable()
 export class TwitchApiService {
@@ -14,7 +15,13 @@ export class TwitchApiService {
   constructor(private http: HttpClient) {}
 
   getCurrentLivesList() {
-    return this.http.get<Live[]>(STREAM_LIST);
+    let headers = new Headers({
+      Authorization: BEARER_AUTH,
+      'Client-Id': CLIENT_ID,
+    });
+
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.get<Live[]>(STREAM_LIST, options);
   }
 }
- 
