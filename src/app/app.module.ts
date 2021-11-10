2a1f1ba9dbf58cfc1c741e6370a4fb80958aa427
MaevaBtd <melizenn@gmail.com>
2021-11-10 14:12:36 +0100

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MiniatureVideoComponent } from './miniature-video/miniature-video.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, MiniatureVideoComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
