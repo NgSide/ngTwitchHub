76b63d29e6e49465525a13b6678a39faff3847f9
Christopher MONTOYA <contact@christopher-montoya.com>
2021-11-11 13:47:33 -0500

    feat(stream-thumbnail): component ready to use

--- src/app/components/stream-thumbnail/stream-thumbnail.component.ts
+++ src/app/components/stream-thumbnail/stream-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stream-thumbnail',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stream-thumbnail.component.scss']
 })
 export class StreamThumbnailComponent implements OnInit {
+
+  @Input() viewers: number = 0;
+  @Input() url: string = 'https://static-cdn.jtvnw.net/previews-ttv/live_user_mizkif-440x248.jpg';
 
   constructor() { }
 
